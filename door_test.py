e76993ff4eb6e67ac1d78fdba1974f62ae19c99f
IM23a-buerkim <buerkim@bzz.ch>
2024-08-21 15:57:46 +0200

    door_test.py fehlre behoben

--- door_test.py
+++ door_test.py
@@ -1,37 +1,66 @@
+"""
+Created on
+"""
 import pytest
 
 from door import Door, DoorLock
 
-
 @pytest.fixture
 def a_door():
+    """
+    Fixture to
+    """
     return Door(DoorLock(), 'green')
 
 
-def test_initial(a_door):
-    assert a_door.color == 'green'
+def test_initial(a_doora):
+    """
+    Test initialization
+    """
+    assert a_doora.color == 'green'
 
 
-def test_get_set_color(a_door):
-    a_door.color = 'red'
-    assert a_door.color == 'red'
+def test_get_set_color(a_doorx):
+    """
+    Testen der Tür farbe
+    """
+    a_doorx.color = 'red'
+    assert a_doorx.color == 'red'
 
 
-def test_door_open(a_door):
-    a_door.open_the_door()
-    assert a_door.door_is_open
+def test_door_open(a_doorb):
+    """
+    Testen der
+    """
+    a_doorb.open_the_door()
+    assert a_doorb.door_is_open
 
 
-def test_door_close(a_door):
-    a_door.close_the_door()
-    assert a_door.door_is_open == False
+def test_door_close(a_doorc):
+    """
+
+    :param a_doorc:
+    :return:
+    """
+    a_doorc.close_the_door()
+    assert a_doorc.door_is_open is False
 
 
-def test_door_locking(a_door):
-    a_door.lock_the_door()
-    assert a_door._door_is_locked
+def test_door_locking(a_doord):
+    """
+
+    :param a_door:
+    :return:
+    """
+    a_doord.lock_the_door()
+    assert a_doord._door_is_locked
 
 
-def test_door_unlock(a_door):
-    a_door.unlock_the_door()
-    assert a_door._door_is_locked == False
+def test_door_unlock(a_doore):
+    """
+
+    :param a_door:
+    :return:
+    """
+    a_doore.unlock_the_door()
+    assert a_doore._door_is_locked is False
